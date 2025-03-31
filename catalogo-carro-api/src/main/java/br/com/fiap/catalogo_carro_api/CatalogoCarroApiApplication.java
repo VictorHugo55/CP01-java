f959173d8e764af1174e5eca9cbfdc65ea9dd64a
VictorHugo55 <davichugopereira@gmail.com>
2025-03-31 11:33:44 -0300

    Inicialização do projeto Spring Boot

---
+++ catalogo-carro-api/src/main/java/br/com/fiap/catalogo_carro_api/CatalogoCarroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.catalogo_carro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoCarroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoCarroApiApplication.class, args);
+	}
+
+}
